b454bab43f1c77152f9dc2e49720ba05b86d6cb4
NovusEdge <novusedge0@gmail.com>
2021-04-09 21:02:44 +0530

    Added colors to the cli

--- setup.py
+++ setup.py
@@ -13,24 +13,31 @@ import os
 PATH = pathlib.Path(__file__).parent.parent.absolute()
 os.chdir(PATH)
 
+COLORS = {
+    "R"     : colorama.Fore.RED,
+    "G"     : colorama.Fore.GREEN,
+    "Y"     : colorama.Fore.YELLOW,
+    "RESET" : colorama.Fore.RESET,
+}
 
 def input_copy_file(dest: str, message: str):
     try:
         src = input(message)
         shutil.copyfile(src, dest)
-        logging.info(f"[+] Found the file {src}!")
+        logging.info(f"{COLORS['G']}[+] Found the file {src}!{COLORS['RESET']}")
     except Exception as e:
-        logging.error(f"[-] Couldn't find the file: {src}\nPlease set it up.")
+        logging.error(f"{COLORS['R']}[-] Couldn't find the file: {src}"
+                      f"Please set it up.{COLORS['RESET']}")
 
 def set_docker_up():
-    docker = input("[*] Would you like to perform a setup for docker as well(y/n)?: ")
+    docker = input(f"{COLORS['Y']}[*] Would you like to perform a setup for docker as well(y/n)?: ")
     if docker in ['y', 'Y']:
-        token = input("[*] Enter token: ")
-        usr_id = input("[*] Enter User ID: ")
+        token = input(f"{COLORS['G']}[+] Enter token: ")
+        usr_id = input(f"{COLORS['G']}[+] Enter User ID: ")
         subprocess.run(f"$(which python3) docker_setup.py -t=\"{token}\ -u=\"{usr_id}\"")
 
     elif docker in ['n', 'N']:
-        print("[*] Skipping docker setup...\nPlease refer to the project docs"
+        print(f"{COLORS['Y']}[*] Skipping docker setup...\nPlease refer to the project docs"
               "for instructions to setup for running the bot in a docker container"
         )
 
@@ -38,22 +45,22 @@ def main():
     if not os.isdir("binance-trade-bot"):
         os.system('git clone https://github.com/edeng23/binance-trade-bot')
 
-    isBTB = input('[*] Is there a BTB installation on your filesystem (y/n)?: ')
+    isBTB = input(f'{COLORS['Y']}[*] Is there a BTB installation on your filesystem (y/n)?: ')
     if isBTB in ['y', 'Y']:
 
         input_copy_file("binance-trade-bot/user.cfg",
-                        "[+] Enter path to user.cfg file: ")
+                        f"{COLORS['G']}[+] Enter path to user.cfg file: ")
 
         input_copy_file("binance-trade-bot/supported_coin_list",
-                        "[+] Enter the path to supported_coin_list: ")
+                        f"{COLORS['G']}[+] Enter the path to supported_coin_list: ")
 
         input_copy_file("binance-trade-bot/config/apprise.yml",
-                        "[+] Enter the path to apprise.yml file: ")
+                        f"{COLORS['G']}[+] Enter the path to apprise.yml file: ")
 
 
     elif isBTB in ['n', 'N']:
 
-        print('''[*] Please manually create the files:
+        print(f'''{COLORS['Y']}[*] Please manually create the files:
               binance-trade-bot/user.cfg
               binance-trade-bot/supported_coin_list
               binance-trade-bot/config/apprise.yml''')
@@ -63,7 +70,7 @@ def main():
 
     set_docker_up()
 
-    print("[*] All set!")
+    print(f"[*] All set!{COLORS['RESET']}")
 
 
 if __name__ == '__main__':
