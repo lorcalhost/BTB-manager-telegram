814492fcf952531e65f046bd8e3acc4e275e22ce
lorcalhost <callegari.lorenzo@gmail.com>
2021-04-23 15:46:18 +0200

    Add percent change in Current value
    As mentioned in #62

--- btb_manager_telegram/buttons.py
+++ btb_manager_telegram/buttons.py
@@ -86,11 +86,12 @@ def current_value():
                     f"\nLast update: `{last_update.strftime('%H:%M:%S %d/%m/%Y')}`\n\n"
                     f"*Current coin {current_coin}:*\n"
                     f"\t\- Balance: `{format_float(balance)}` *{current_coin}*\n"
-                    f"\t\- Current coin exchange ratio: `{format_float(usd_price)}` *USD*/*{current_coin}*\n"
+                    f"\t\- Current coin exchange rate: `{format_float(usd_price)}` *USD*/*{current_coin}*\n"
                     f"\t\- Value in *USD*: `{round(balance * usd_price, 2)}` *USD*\n"
                     f"\t\- Value in *BTC*: `{format_float(balance * btc_price)}` *BTC*\n\n"
-                    f"_Initially bought for_ {round(buy_price, 2)} *{bridge}*\n"
-                    f"_Exchange ratio when purchased:_ `{format_float(buy_price / alt_amount)}` *{bridge}*/*{current_coin}*".replace(
+                    f"_Initially bought for_ `{round(buy_price, 2)}` *{bridge}*\n"
+                    f"_Exchange rate when purchased:_ `{format_float(buy_price / alt_amount)}` *{bridge}*/*{current_coin}*\n"
+                    f"{f'*Change in value*: `{round((balance * usd_price - buy_price) / buy_price * 100, 2)}` *%*' if bridge in ['USDT', 'BUSD'] else ''}".replace(
                         ".", "\."
                     )
                 ]
