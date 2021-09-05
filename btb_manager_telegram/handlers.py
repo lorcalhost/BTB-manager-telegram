250addadbc27ba55df7ede4fc72dbd062eda9dbe
karl.hudgell <karl.hudgell@bjss.com>
2021-09-05 13:44:39 +0100

    formatting

--- btb_manager_telegram/handlers.py
+++ btb_manager_telegram/handlers.py
@@ -39,7 +39,7 @@ from btb_manager_telegram.utils import (
     get_custom_scripts_keyboard,
     kill_btb_manager_telegram_process,
     telegram_text_truncator,
-    load_custom_settings
+    load_custom_settings,
 )
 
 
@@ -100,15 +100,14 @@ def menu(update: Update, _: CallbackContext) -> int:
             update.message.reply_text(
                 mes, reply_markup=reply_markup, parse_mode="MarkdownV2"
             )
-        try:    
+        try:
             if load_custom_settings()["Total_Binance_Wallet_Balance"] == True:
                 for mes in buttons.wallet_value():
                     update.message.reply_text(
                         mes, reply_markup=reply_markup, parse_mode="MarkdownV2"
                     )
         except:
-            print('Setting not found')            
-        
+            print("Setting not found")
 
     elif update.message.text == "🚨 Panic button":
         message, status = buttons.panic_btn()
