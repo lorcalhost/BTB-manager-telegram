1ab596d1549cb4a115122df22fccf5165f024d88
ullasbharadwaj <ullasbharadwaj13@gmail.com>
2022-01-14 21:05:25 +0100

    black whack

--- btb_manager_telegram/keyboards.py
+++ btb_manager_telegram/keyboards.py
@@ -4,21 +4,21 @@ import i18n
 
 menu = ReplyKeyboardMarkup(
     [
-        [i18n.t("keyboard.current_value"),  i18n.t("keyboard.bot_stats")],
+        [i18n.t("keyboard.current_value"), i18n.t("keyboard.bot_stats")],
         [i18n.t("keyboard.current_ratios"), i18n.t("keyboard.next_coin")],
-        [i18n.t("keyboard.trade_history"),  i18n.t("keyboard.progress")],
-        [i18n.t("keyboard.graph"),          i18n.t("keyboard.configurations")],
-        [i18n.t("keyboard.check_status"),   i18n.t("keyboard.maintenance")],
+        [i18n.t("keyboard.trade_history"), i18n.t("keyboard.progress")],
+        [i18n.t("keyboard.graph"), i18n.t("keyboard.configurations")],
+        [i18n.t("keyboard.check_status"), i18n.t("keyboard.maintenance")],
     ],
     resize_keyboard=True,
 )
 
 config = ReplyKeyboardMarkup(
     [
-        [i18n.t("keyboard.start"),      i18n.t("keyboard.stop")],
-        [i18n.t("keyboard.read_logs"),  i18n.t("keyboard.delete_db")],
-        [i18n.t("keyboard.edit_cfg"),   i18n.t("keyboard.edit_coin_list")],
-        [i18n.t("keyboard.export_db"),  i18n.t("keyboard.back")],
+        [i18n.t("keyboard.start"), i18n.t("keyboard.stop")],
+        [i18n.t("keyboard.read_logs"), i18n.t("keyboard.delete_db")],
+        [i18n.t("keyboard.edit_cfg"), i18n.t("keyboard.edit_coin_list")],
+        [i18n.t("keyboard.export_db"), i18n.t("keyboard.back")],
     ],
     resize_keyboard=True,
 )
