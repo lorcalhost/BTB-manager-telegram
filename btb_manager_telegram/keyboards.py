4811de39b2abbadf4cddbaa1c25315f2e07c613e
ullasbharadwaj <ullasbharadwaj13@gmail.com>
2022-01-16 15:53:02 +0100

    merge error

--- btb_manager_telegram/keyboards.py
+++ btb_manager_telegram/keyboards.py
@@ -8,7 +8,7 @@ menu = ReplyKeyboardMarkup(
         [i18n.t("keyboard.current_ratios"), i18n.t("keyboard.next_coin")],
         [i18n.t("keyboard.trade_history"), i18n.t("keyboard.progress")],
         [i18n.t("keyboard.graph"), i18n.t("keyboard.configurations")],
-        [i18n.t("keyboard.coin_forecast")]
+        [i18n.t("keyboard.coin_forecast")],
         [i18n.t("keyboard.check_status"), i18n.t("keyboard.maintenance")],
     ],
     resize_keyboard=True,
